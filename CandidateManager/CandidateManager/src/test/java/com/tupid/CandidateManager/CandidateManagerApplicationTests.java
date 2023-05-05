d40218e111039c9342594ac7eb711b10675ec902
Deepak Gupta <deepakgupta71628@gmail.com>
2023-05-05 14:32:07 +0530

    Ap created

---
+++ CandidateManager/CandidateManager/src/test/java/com/tupid/CandidateManager/CandidateManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tupid.CandidateManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
