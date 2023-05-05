f99a11d0f74debfecab3344bdbc72ffd7895f4c2
Deepak Gupta <deepakgupta71628@gmail.com>
2023-05-05 14:55:16 +0530

    recreated

--- candidateManager/src/main/java/com/tupid/candidateManager/CandidateManagerApplication.java
+++ candidateManager/src/main/java/com/tupid/candidateManager/CandidateManagerApplication.java
@@ -1,4 +1,4 @@
-package com.tupid.CandidateManager;
+package com.tupid.candidateManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- candidateManager/src/test/java/com/tupid/candidateManager/CandidateManagerApplicationTests.java
+++ candidateManager/src/test/java/com/tupid/candidateManager/CandidateManagerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tupid.CandidateManager;
+package com.tupid.candidateManager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
